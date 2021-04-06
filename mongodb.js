a2f5972db586e1da7ab6297c5fa815c626737170
TheBigR <nevoroy@gmail.com>
2021-04-06 12:43:57 +0300

    more mongo tryouts

--- mongodb.js
+++ mongodb.js
@@ -26,20 +26,54 @@ MongoClient.connect(
     //     console.log(users)
     //   })
 
-    db.collection('tasks').findOne(
-      { _id: new ObjectID('606864f84b27f548d453f393') },
-      (error, task) => {
-        if (error) {
-          return console.log('Unable to fetch')
-        }
-        console.log(task)
-      },
-    )
+    // db.collection('tasks').findOne(
+    //   { _id: new ObjectID('606864f84b27f548d453f393') },
+    //   (error, task) => {
+    //     if (error) {
+    //       return console.log('Unable to fetch')
+    //     }
+    //     console.log(task)
+    //   },
+    // )
+
+    // db.collection('tasks')
+    //   .find({ result: false })
+    //   .toArray((error, tasks) => {
+    //     console.log(tasks)
+    //   })
+
+    // db.collection('users')
+    //   .updateOne(
+    //     {
+    //       _id: new ObjectID('60698ed39099a72984333aea'),
+    //     },
+    //     {
+    //       $set: {
+    //         age: 5,
+    //       },
+    //     },
+    //   )
+    //   .then((result) => {
+    //     console.log(result)
+    //   })
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
+
+    // db.collection('tasks')
+    //   .updateMany({ result: false }, { $set: { result: true } })
+    //   .then((result) => console.log(result))
+    //   .catch((error) => {
+    //     console.log(error)
+    //   })
 
     db.collection('tasks')
-      .find({ result: false })
-      .toArray((error, tasks) => {
-        console.log(tasks)
+      .deleteOne({ description: 'finish task' })
+      .then((result) => {
+        console.log(result)
+      })
+      .catch((error) => {
+        console.log(error)
       })
   },
 )
