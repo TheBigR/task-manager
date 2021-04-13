a640480d74d9cc8043ef24c365c25441fa325fcc
TheBigR <nevoroy@gmail.com>
2021-04-13 08:55:12 +0300

    enabled cors

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT 
+const port = process.env.PORT
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*') // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  )
+  next()
+})
 
 app.use(express.json())
 app.use(userRouter)
