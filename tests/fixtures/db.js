f45dd7a27fff9fccb4a6faa6058f9d925736c3ae
TheBigR <nevoroy@gmail.com>
2021-04-21 14:30:15 +0300

    refactored tests and added more.

---
+++ tests/fixtures/db.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const User = require('../../src/models/user')
+
+const userOneId = new mongoose.Types.ObjectId()
+
+const userOne = {
+  _id: userOneId,
+  name: 'dugma user',
+  email: 'samplemail233@hotmail.com',
+  password: 'dugma123!',
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET),
+    },
+  ],
+}
+
+const setupDatabase = async () => {
+  await User.deleteMany()
+  await new User(userOne).save()
+}
+
+module.exports = {
+  userOneId,
+  userOne,
+  setupDatabase,
+}
